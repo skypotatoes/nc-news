55dc66051a31f0e86ca3c332f3f86fd0a94713f7
skypotatoes <ronan.dodds@gmail.com>
2022-02-17 14:12:49 +0000

    corrected repeated lines of code in app.js

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require('express')
 const app = express()
 
-
-const {getTopics, patchVotesByArticleId, getArticleById,getUsers} = require('./controllers')
-
+const {
+  getTopics,
+  patchVotesByArticleId,
+  getArticleById,
+  getUsers,
+} = require('./controllers')
 
 app.use(express.json())
 
 app.get('/api/topics', getTopics)
 app.get('/api/articles/:article_id', getArticleById)
 
-
 app.get('/api/users', getUsers)
 
 app.patch('/api/articles/:article_id', patchVotesByArticleId)
-
-app.all('/*', (req, res) => {
-    res.status(404).send({ msg: 'Path not found' });
-  });
-
 
 app.all('/*', (req, res) => {
   res.status(404).send({ msg: 'Path not found' })
